59aac85a95b952634c11e6af16d68513e13254b4
Francisquet Vargas <fvs00725@gmail.com>
2023-04-04 21:43:21 -0500

    added get request to fetch a book by title

--- books.py
+++ books.py
@@ -14,3 +14,9 @@ BOOKS = [
 @app.get("/books")
 async def get_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def get_book_by_title(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
